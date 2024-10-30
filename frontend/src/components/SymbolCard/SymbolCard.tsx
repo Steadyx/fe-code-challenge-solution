2521abd4fd2938ebb65fb30d265757047dff58cd
Edward Kemp <edwardlanekemp@gmail.com>
2024-10-30 17:20:32 +0000

    refactor(SymbolCard): Extract header to separate component

--- frontend/src/components/SymbolCard/SymbolCard.tsx
+++ frontend/src/components/SymbolCard/SymbolCard.tsx
@@ -1,6 +1,7 @@
 import './symbolCard.css';
 import { ReactComponent as CompanyIcon } from '@/assets/company.svg';
 import { useAppSelector } from '@/hooks/redux';
+import SymbolCardHeader from './SymbolCardHeader'
 import ListItem from '@/components/ListItem';
 
 type SymbolCardProps = {
@@ -11,14 +12,13 @@ type SymbolCardProps = {
 
 const SymbolCard = ({ id, onClick, price }: SymbolCardProps) => {
   const { trend, companyName } = useAppSelector((state) => state.stocks.entities[id]);
+
   const handleOnClick = () => {
     onClick(id);
   };
   return (
     <div onClick={handleOnClick} className="symbolCard">
-      <div>
-        {id} - {trend}
-      </div>
+      <SymbolCardHeader id={id} />
       <div>Price:</div>
       <div>{price || '--'} </div>
       <ListItem Icon={<CompanyIcon />} label={companyName} />
